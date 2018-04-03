dd342afea5bd6ba7ba6879107b24b175bc6cd0c5
Jason Cramer <jason.t.cramer@gmail.com>
2018-04-03 09:27:00 -0400

    Create sample directory

--- utils.py
+++ utils.py
@@ -6,7 +6,11 @@ def save_samples(epoch_samples, epoch, output_dir, fs=16000):
     """
     Save output samples to disk
     """
+    sample_dir = os.path.join(output_dir, str(epoch))
+    if not os.path.exists(sample_dir):
+        os.makedirs(sample_dir)
+
     for idx, samp in enumerate(epoch_samples):
-        output_path = os.path.join(output_dir, str(epoch), "{}.wav".format(idx+1))
+        output_path = os.path.join(sample_dir, "{}.wav".format(idx+1))
         samp = samp[0]
         librosa.output.write_wav(output_path, samp, fs)
